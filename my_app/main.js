54b9f82d1f94d2573ea1de18a78c2e553675a380
Konstantinos Fevgas <kon.fevgas@gmail.com>
2024-11-01 11:39:41 +0200

    my own working version

--- my_app/main.js
+++ my_app/main.js
@@ -12,99 +12,31 @@ const osmTile = new TileLayer({
 });
 
 // Variables
-const geoserverURL = 'http://localhost:8080/geoserver/wms'
-const isLayerVisible = false
+const _lundProjectGeoserverURL = 'http://localhost:8080/geoserver/wms'
+const _isLayerVisible = false
 
 // GeoServer WMS Layers
-const lundAddresses = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:addresses_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
+function createWMSLayer (layerName, geoserverURL = _lundProjectGeoserverURL, isVisible = _isLayerVisible) {
+  return new ImageLayer ({
+    source: new ImageWMS({
+      url: geoserverURL,
+      params: {
+        'LAYERS': `lund_web_mapping:${layerName}`,
+        'TILED': true
+      },
+    }),
+    visible: isVisible,
+  })
+}
 
-const lundDistricts = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:districts_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
-
-const lundPublicBuildings = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:public_buildings_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
-
-const lundRailroads = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:railroads_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
-
-const lundRoadsAll = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:roads_all_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
-
-const lundRoadsHighway = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:roads_highway_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
-
-
-const lundRoadsThrough = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:roads_throug_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
-
-const lundRuralBuildings = new ImageLayer({
-  source: new ImageWMS({
-    url: geoserverURL,
-    params: {
-      'LAYERS': 'lund_web_mapping:rural_buildings_wgs84',
-      'TILED': true
-    },
-  }),
-  visible: isLayerVisible,
-});
-
+const lundAddresses = createWMSLayer('addresses_wgs84');
+const lundDistricts = createWMSLayer('districts_wgs84');
+const lundPublicBuildings = createWMSLayer('public_buildings_wgs84');
+const lundRailroads = createWMSLayer('railroads_wgs84');
+const lundRoadsAll = createWMSLayer('roads_all_wgs84');
+const lundRoadsHighway = createWMSLayer('roads_highway_wgs84');
+const lundRoadsThrough = createWMSLayer('roads_throug_wgs84');
+const lundRuralBuildings = createWMSLayer('rural_buildings_wgs84');
 
 // Set up the map view
 const mapView = new View({
@@ -130,46 +62,23 @@ const map = new Map({
 });
 
 
-
-
 // Toggle Layers
+const layerToggles = [
+  { id: 'addresses-toggle', layer: lundAddresses },
+  { id: 'districts-toggle', layer: lundDistricts },
+  { id: 'publicBuildings-toggle', layer: lundPublicBuildings },
+  { id: 'ruralBuildings-toggle', layer: lundRuralBuildings },
+  { id: 'roadsAll-toggle', layer: lundRoadsAll },
+  { id: 'roadsHighway-toggle', layer: lundRoadsHighway },
+  { id: 'roadsThrough-toggle', layer: lundRoadsThrough },
+  { id: 'railroads-toggle', layer: lundRailroads }
+];
 
-const addressesToggle = document.getElementById('addresses-toggle');
-addressesToggle.addEventListener('change', function () {
-  lundAddresses.setVisible(addressesToggle.checked);
+// Add event listeners to each toggle checkbox
+layerToggles.forEach(({ id, layer }) => {
+  const checkbox = document.getElementById(id);
+  checkbox.addEventListener('change', () => {
+    layer.setVisible(checkbox.checked);
+  });
 });
 
-const districtsToggle = document.getElementById('districts-toggle');
-districtsToggle.addEventListener('change', function () {
-  lundDistricts.setVisible(districtsToggle.checked);
-});
-
-const publicBuildingsToggle = document.getElementById('publicBuildings-toggle');
-publicBuildingsToggle.addEventListener('change', function () {
-  lundPublicBuildings.setVisible(publicBuildingsToggle.checked);
-});
-
-const ruralBuildingsToggle = document.getElementById('ruralBuildings-toggle');
-ruralBuildingsToggle.addEventListener('change', function () {
-  lundRuralBuildings.setVisible(ruralBuildingsToggle.checked);
-});
-
-const roadsAllToggle = document.getElementById('roadsAll-toggle');
-roadsAllToggle.addEventListener('change', function () {
-  lundRoadsAll.setVisible(roadsAllToggle.checked);
-});
-
-const roadsHighwayToggle = document.getElementById('roadsHighway-toggle');
-roadsHighwayToggle.addEventListener('change', function () {
-  lundRoadsHighway.setVisible(roadsHighwayToggle.checked);
-});
-
-const roadsThroughToggle = document.getElementById('roadsThrough-toggle');
-roadsThroughToggle.addEventListener('change', function () {
-  lundRoadsThrough.setVisible(roadsThroughToggle.checked);
-});
-
-const railroadsToggle = document.getElementById('railroads-toggle');
-railroadsToggle.addEventListener('change', function () {
-  lundRailroads.setVisible(railroadsToggle.checked);
-});
